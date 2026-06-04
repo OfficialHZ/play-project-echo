f9da4b966126d4aa727c0aed096abe691009af6c
HZ <TRHZUSER@gmail.com>
2026-06-03 22:11:29 -0600

    feat: implement first person camera controls

--- game/play-godot-learning/Scripts/Player.cs
+++ game/play-godot-learning/Scripts/Player.cs
@@ -4,6 +4,43 @@ public partial class Player : CharacterBody3D
 {
 	private const float Speed = 5.0f;
 	private const float Gravity = 20.0f;
+	private const float MouseSensitivity = 0.0025f;
+
+	private Node3D _cameraPivot;
+
+	public override void _Ready()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+
+		_cameraPivot = GetNode<Node3D>("CameraPivot");
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		if (@event.IsActionPressed("ui_cancel"))
+		{
+			Input.MouseMode = Input.MouseModeEnum.Visible;
+		}
+
+		if (@event is InputEventMouseMotion mouseMotion)
+		{
+			RotateY(-mouseMotion.Relative.X * MouseSensitivity);
+
+			_cameraPivot.RotateX(
+				-mouseMotion.Relative.Y * MouseSensitivity
+			);
+
+			Vector3 rotation = _cameraPivot.Rotation;
+
+			rotation.X = Mathf.Clamp(
+				rotation.X,
+				Mathf.DegToRad(-89),
+				Mathf.DegToRad(89)
+			);
+
+			_cameraPivot.Rotation = rotation;
+		}
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -21,16 +58,15 @@ public partial class Player : CharacterBody3D
             "move_backward"
 		);
 
-		Vector3 direction = new Vector3(
-			input.X,
-			0,
-			input.Y
-		);
+		Vector3 direction =
+			Transform.Basis *
+			new Vector3(input.X, 0, input.Y);
 
 		velocity.X = direction.X * Speed;
 		velocity.Z = direction.Z * Speed;
 
 		Velocity = velocity;
+
 		MoveAndSlide();
 	}
 }
