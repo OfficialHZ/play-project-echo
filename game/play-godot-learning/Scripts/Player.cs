8300bd1643205143d920256243ea6d3533ef6b45
HZ <TRHZUSER@gmail.com>
2026-06-03 22:07:20 -0600

    feat: add basic WASD movement

--- game/play-godot-learning/Scripts/Player.cs
+++ game/play-godot-learning/Scripts/Player.cs
@@ -2,6 +2,7 @@ using Godot;
 
 public partial class Player : CharacterBody3D
 {
+	private const float Speed = 5.0f;
 	private const float Gravity = 20.0f;
 
 	public override void _PhysicsProcess(double delta)
@@ -12,6 +13,22 @@ public partial class Player : CharacterBody3D
 		{
 			velocity.Y -= Gravity * (float)delta;
 		}
+
+		Vector2 input = Input.GetVector(
+			"move_left",
+			"move_right",
+			"move_forward",
+            "move_backward"
+		);
+
+		Vector3 direction = new Vector3(
+			input.X,
+			0,
+			input.Y
+		);
+
+		velocity.X = direction.X * Speed;
+		velocity.Z = direction.Z * Speed;
 
 		Velocity = velocity;
 		MoveAndSlide();
